47e2e2a10ec8aea373b024720ce46f65c2d892a2
SandiswaShange <sandiswashange32@gmail.com>
2026-05-27 04:49:22 +0200

    feat: updated README to be relevant

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+137711fa9c1a449963996ddb4e6400b241444761")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f394802f4116ccdbd4734a7ddb0b55cc7a516d99")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
