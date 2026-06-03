1e4e701727600bbbebcaf5578de7f8b77bfe735f
SandiswaShange <sandiswashange32@gmail.com>
2026-06-03 22:36:19 +0200

    "cache I had to commit"

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b007c886bbc0acfc06b831d9496497fcc6092544")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0be273930e3a3c66f96b9805342c85ac4d8f0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
