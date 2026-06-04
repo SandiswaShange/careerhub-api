1b63b3a00cf0ddb2346ccfc6d9bfafec0279eca2
SandiswaShange <sandiswashange32@gmail.com>
2026-06-04 02:55:18 +0200

    "cache stuff"

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0be273930e3a3c66f96b9805342c85ac4d8f0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4e701727600bbbebcaf5578de7f8b77bfe735f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
