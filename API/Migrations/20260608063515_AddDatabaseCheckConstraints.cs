be24e7c2443faa303559b213e7ce7cb0f9c4fdbe
SandiswaShange <sandiswashange32@gmail.com>
2026-06-08 09:10:06 +0200

    feat: added migrations for Postgre

---
+++ API/Migrations/20260608063515_AddDatabaseCheckConstraints.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDatabaseCheckConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddCheckConstraint(
+                name: "ck_job_listings_closing_after_posted",
+                table: "job_listings",
+                sql: "\"ClosingDate\" > \"PostedAt\"");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "ck_job_listings_min_salary_positive",
+                table: "job_listings",
+                sql: "\"MinSalary\" IS NULL OR \"MinSalary\" > 0");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "ck_job_listings_salary_range",
+                table: "job_listings",
+                sql: "\"MinSalary\" IS NULL OR \"MaxSalary\" IS NULL OR \"MaxSalary\" > \"MinSalary\"");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "ck_applications_submitted_not_future",
+                table: "applications",
+                sql: "\"SubmittedAt\" <= CURRENT_TIMESTAMP");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "ck_job_listings_closing_after_posted",
+                table: "job_listings");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "ck_job_listings_min_salary_positive",
+                table: "job_listings");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "ck_job_listings_salary_range",
+                table: "job_listings");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "ck_applications_submitted_not_future",
+                table: "applications");
+        }
+    }
+}
--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef3b1028331e5c7a6d293effe183046e8b8542fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8897b7cd4c0a5ed59c2fa471e6f76cd305b47b1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
