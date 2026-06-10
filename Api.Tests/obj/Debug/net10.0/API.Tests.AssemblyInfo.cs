432d02052e582bded38a216f238ded463b933da3
SandiswaShange <sandiswashange32@gmail.com>
2026-06-10 02:29:55 +0200

    "Ran first four commands in CLI to import packages"

--- Api.Tests/obj/Debug/net10.0/API.Tests.AssemblyInfo.cs
+++ Api.Tests/obj/Debug/net10.0/API.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b56c3e52c6ccfd74ee4ffac063c25e9de98c5f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3529eff747a6c5684f9495ef439e68fe2ca04b30")]
 [assembly: System.Reflection.AssemblyProductAttribute("API.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
