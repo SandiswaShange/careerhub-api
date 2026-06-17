7ca82c16e6a3525fdaf0691e4dbd72f2221b0bd7
SandiswaShange <sandiswashange32@gmail.com>
2026-06-17 10:35:13 +0200

    feat: Part 2

---
+++ careerhub-frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">
+        CareeHub API frontend
+      </h1>
+    </main>
+  );
+}
