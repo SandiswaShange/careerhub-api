685b1d8b84b7e5238bbd5493f1063bd331bd18ce
SandiswaShange <sandiswashange32@gmail.com>
2026-07-06 09:50:07 +0200

    feat: Company logo and hero svgs added

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+445b0ec49b2d4e486e010b3a82779b1538b6474c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d555e450421ab8082e4d88f9b2242a187650f6b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
